41e6cfce8bf34163cb7b15d821fc1be8e38c34a5
meeth03 <meeeth0304yadav@gmail.com>
2022-08-03 14:37:49 +0530

    comi

--- src/main/java/dummy_framework_git/Git_Command.java
+++ src/main/java/dummy_framework_git/Git_Command.java
@@ -5,6 +5,9 @@ public class Git_Command {
 	public static void main(String[]args){
 		System.out.println("hello");
 		System.out.println("hi");
+		System.out.println("hello");
+		System.out.println("hi");
+		
 	}
 
 }
